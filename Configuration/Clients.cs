20e609ff173043145476934c9b89aadb6e4bba29
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2016-04-03 01:14:35 +0200

    changed client flow

--- Configuration/Clients.cs
+++ Configuration/Clients.cs
@@ -10,118 +10,6 @@ namespace Microbrewit.AuthServer.Configuration
             return new List<Client>
             {
                 ///////////////////////////////////////////
-                // Console Client Credentials Flow Sample
-                //////////////////////////////////////////
-                new Client
-                {
-                    ClientId = "client",
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    Flow = Flows.ClientCredentials,
-
-                    AllowedScopes = new List<string>
-                    {
-                        "api1", "api2"
-                    },
-                },
-
-                ///////////////////////////////////////////
-                // Console Resource Owner Flow Sample
-                //////////////////////////////////////////
-                new Client
-                {
-                    ClientId = "roclient",
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    Flow = Flows.ResourceOwner,
-
-                    AllowedScopes = new List<string>
-                    {
-                        StandardScopes.OpenId.Name,
-                        StandardScopes.Email.Name,
-                        StandardScopes.OfflineAccess.Name,
-
-                        "api1", "api2"
-                    }
-                },
-
-                ///////////////////////////////////////////
-                // Console Client Credentials Flow Sample
-                //////////////////////////////////////////
-                new Client
-                {
-                    ClientId = "client.custom",
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    Flow = Flows.Custom,
-                    
-                    AllowedCustomGrantTypes = new List<string>
-                    {
-                        "custom"
-                    },
-
-                    AllowedScopes = new List<string>
-                    {
-                        "api1", "api2"
-                    }
-                },
-
-                ///////////////////////////////////////////
-                // Introspection Client Sample
-                //////////////////////////////////////////
-                new Client
-                {
-                    ClientId = "roclient.reference",
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    Flow = Flows.ResourceOwner,
-
-                    AllowedScopes = new List<string>
-                    {
-                        "api1", "api2"
-                    },
-
-                    AccessTokenType = AccessTokenType.Reference
-                },
-
-                ///////////////////////////////////////////
-                // MVC Implicit Flow Samples
-                //////////////////////////////////////////
-                new Client
-                {
-                    ClientId = "mvc_implicit",
-                    ClientName = "MVC Implicit",
-                    ClientUri = "http://identityserver.io",
-                    Flow = Flows.Implicit,
-                    RedirectUris = new List<string>
-                    {
-                        "http://localhost:44077/signin-oidc"
-                    },
-
-                    AllowedScopes = new List<string>
-                    {
-                        StandardScopes.OpenId.Name,
-                        StandardScopes.Profile.Name,
-                        StandardScopes.Email.Name,
-                        StandardScopes.Roles.Name,
-
-                        "api1", "api2"
-                    }
-                },
-
-                ///////////////////////////////////////////
                 // JS OAuth 2.0 Sample
                 //////////////////////////////////////////
                 new Client
@@ -147,35 +35,26 @@ namespace Microbrewit.AuthServer.Configuration
                     }
                 },
                 
-                ///////////////////////////////////////////
-                // JS OIDC Sample
+                  ///////////////////////////////////////////
+                // Microbrewit
                 //////////////////////////////////////////
                 new Client
                 {
-                    ClientId = "js_oidc",
-                    ClientName = "JavaScript OIDC Client",
-                    ClientUri = "http://identityserver.io",
+                    ClientId = "microbrewit",
+                    ClientName = "Microbrewit",
+                    ClientUri = "http://microbrew.it",
+                    
+                    Flow = Flows.ClientCredentials,
                     ClientSecrets = new List<Secret>
                     {
                         new Secret("secret".Sha256())
                     },
-                    Flow = Flows.Implicit,
                     RedirectUris = new List<string>
                     {
-                        "http://localhost:7017/index.html",
-                        "http://localhost:7017/silent_renew.html",
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "http://localhost:7017/index.html",
+                        "http://localhost:3000/"
                     },
 
-                    AllowedCorsOrigins = new List<string>
-                    {
-                        "http://localhost:7017"
-                    },
-
-                    AllowedScopes = new List<string>
+                     AllowedScopes = new List<string>
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Profile.Name,
@@ -185,7 +64,7 @@ namespace Microbrewit.AuthServer.Configuration
                         "microbrewit-api"
                     }
                 },
-
+                
                  ///////////////////////////////////////////
                 // Postman 
                 //////////////////////////////////////////
@@ -193,7 +72,7 @@ namespace Microbrewit.AuthServer.Configuration
                 {
                     ClientId = "postman",
                     ClientName = "PostMan",
-                    ClientUri = "http://identityserver.io",
+                    ClientUri = "http://microbrew.it",
                     ClientSecrets = new List<Secret>
                     {
                         new Secret("secret".Sha256())
